a58d57fba3e9f016c8d15c16b0e1bcb58c0d4823
Puneet <xpntshglx@gmail.com>
2016-01-13 01:00:24 +0530

    l 5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,48 +1,51 @@
+<!DOCTYPE html>
 <html>
-	<head>
-		<title>Laravel</title>
-		
-		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+    <head>
+        <title>Laravel</title>
 
-		<style>
-			body {
-				margin: 0;
-				padding: 0;
-				width: 100%;
-				height: 100%;
-				color: #B0BEC5;
-				display: table;
-				font-weight: 100;
-				font-family: 'Lato';
-			}
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
-			.container {
-				text-align: center;
-				display: table-cell;
-				vertical-align: middle;
-			}
+        <style>
+            html, body {
+                height: 100%;
+            }
 
-			.content {
-				text-align: center;
-				display: inline-block;
-			}
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
 
-			.title {
-				font-size: 96px;
-				margin-bottom: 40px;
-			}
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
 
-			.quote {
-				font-size: 24px;
-			}
-		</style>
-	</head>
-	<body>
-		<div class="container">
-			<div class="content">
-				<div class="title">Puneet</div>
-				<div class="quote">{{ Inspiring::quote() }}</div>
-			</div>
-		</div>
-	</body>
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 96px;
+            }
+        </style>
+    </head>
+    <body>
+
+
+
+<!-- <div><a class="btn" type="button" href="http://example.com"> kyg login </a></div>
+ -->
+
+        <div class="container">
+            <div class="content">
+                <a class="btn" type="button" href="http://example.com"> kyg login </a><
+            </div>
+        </div>
+    </body>
 </html>
